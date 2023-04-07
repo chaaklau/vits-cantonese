aea31b7468a86fb4792932895b4e2abcd0c3656b
Keith Hon <keith.wkhon@gmail.com>
2023-04-07 14:47:23 +0800

    updated codes cantonese inference

--- text/symbols.py
+++ text/symbols.py
@@ -9,9 +9,9 @@ _letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧ↓↑ '
 '''
 
 # japanese_cleaners2
-_pad        = '_'
-_punctuation = ',.!?-~…'
-_letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧʦ↓↑ '
+# _pad        = '_'
+# _punctuation = ',.!?-~…'
+# _letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧʦ↓↑ '
 
 
 '''# korean_cleaners
@@ -62,11 +62,11 @@ _punctuation = ',.!?…'
 _letters = 'abdfghiklmnopstuvyzøŋȵɑɔɕəɤɦɪɿʑʔʰ̩̃ᴀᴇ15678 '
 '''
 
-'''# chinese_dialect_cleaners
+# chinese_dialect_cleaners
 _pad        = '_'
 _punctuation = ',.!?~…─'
 _letters = '#Nabdefghijklmnoprstuvwxyzæçøŋœȵɐɑɒɓɔɕɗɘəɚɛɜɣɤɦɪɭɯɵɷɸɻɾɿʂʅʊʋʌʏʑʔʦʮʰʷˀː˥˦˧˨˩̥̩̃̚ᴀᴇ↑↓∅ⱼ '
-'''
+
 
 # Export all symbols:
 symbols = [_pad] + list(_punctuation) + list(_letters)
